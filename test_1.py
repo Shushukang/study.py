af4476e52820e7cdba9171456bbd40f819d45d38
shushu_kang <hkang315@gmail.com>
2025-01-11 15:54:23 +0900

    Hellow World!

---
+++ test_1.py
@@ -0,0 +1,8 @@
+import numpy as np
+import pandas as pd
+
+num1 = [10, 20, 30, 20, 10]
+num2 = [1, 2, 3, 4, 5]
+str = ['오늘은','날씨가','흐립니다']
+
+print(num1+num2)
--- test_dog.py
+++ test_dog.py
@@ -3,3 +3,5 @@ print('|q p|   /')
 print('( 0 )"""\ ')
 print('|"^"`    |')
 print('||_/=\\__|')
+
+print("it's a dog")
